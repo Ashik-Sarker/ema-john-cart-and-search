af0eff59b426a2d1f68627cd01b49bb45eeb6656
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-03-28 15:41:56 +0600

    complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+
+    
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + (product.price * product.quantity);
+        shipping = shipping + product.shipping;
+        // console.log(quantity);
+    }
+    // const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * .1).toFixed(2));
+    const grandTotal = (total + shipping + tax).toFixed(2);
     return (
-        <div>
-            <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+        <div className='cart'>
+            <h3>Order Summary</h3>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price :{total}</p>
+            <p>Shipping Price: {shipping}</p>
+            <p>tax: {tax}</p>
+            <h5>Grand Total: {grandTotal}</h5>
         </div>
     );
 };
